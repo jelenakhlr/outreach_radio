29316119df29422da0443add6eb0aebe2b644488
Jelena Petereit <108803123+jelenakhlr@users.noreply.github.com>
2024-05-29 10:57:11 +0200

    amazing changes

--- radio_antennas_masterclass.py
+++ radio_antennas_masterclass.py
@@ -17,7 +17,6 @@ def on_click(event):
     transmitter_x, transmitter_y = transmitter[0], transmitter[1]
     clicked_x = event.xdata
     clicked_y = event.ydata
-    print(clicked_x)
     distance_to_transmitter = abs(clicked_x - transmitter_x) + abs(clicked_y - transmitter_y)
     threshold = 50.0  # Adjust this value based on your plot scale and desired sensitivity
     
@@ -37,36 +36,32 @@ def on_click(event):
         verticalalignment = 'top' , bbox = props)
     return 
 
-def distance(transmitter, gp300):
-    antennas = pd.DataFrame(columns=['position_x', 'position_y', 'distance_to_transmitter', 'signal_strength'])
-    antenna_position_x = [:,2]
-    antenna_position_y = [:,3]
-    N = len(antenna_position_x)-1
-    for station in range(N):
-        position_x = antenna_position_x[station]*10**-4
-        position_y = antenna_position_y[station]*10**-4
-        position_xy = np.array([position_x, position_y])
-        distance   = np.sqrt((transmitter[0]-position_x)**2 + (transmitter[1]-position_y)**2)
-        signal_strength = 1/distance
-        antennas = antennas.append({
-            'position_x' : position_x, 
-            'position_y' : position_y, 
-            'distance_to_transmitter' : distance, 
-            'signal_strength' : signal_strength
-            }, ignore_index=True)
-    return antennas
+def signal_strength(antenna_position_x, antenna_position_y):
+    distance   = np.sqrt((transmitter[0]-antenna_position_x)**2 + (transmitter[1]-antenna_position_y)**2)
+    signal_strength = 1/distance
+    return signal_strength
 
-gp300 = np.genfromtxt("real.list")
+AntennaPosition1 = np.array([5,15])
+AntennaPosition2 = np.array([15,30])
+AntennaPosition3 = np.array([15,5]) 
+print(AntennaPosition1, AntennaPosition2, AntennaPosition3)
 
-transmitter = np.array([random.random()*100, random.random()*100])
-receivers_df = distance(transmitter, gp300)
+antennas = pd.DataFrame({
+    'position_x': np.array([AntennaPosition1[0], AntennaPosition2[0], AntennaPosition3[0]]),
+    'position_y': np.array([AntennaPosition1[1], AntennaPosition2[1], AntennaPosition3[1]])
+})
 
-AntennaPosition1 = np.array([random.random()*100, random.random()*100])
-AntennaPosition2 = np.array([random.random()*100, random.random()*100])
-AntennaPosition3 = np.array([random.random()*100, random.random()*100]) 
+transmitter = np.array([np.random.randint(0,35), np.random.randint(0,35)])
+receivers_df = pd.DataFrame({
+    'position_x': antennas['position_x'],
+    'position_y': antennas['position_y'],
+    'signal_strength': np.array([signal_strength(AntennaPosition1[0], AntennaPosition1[1]), signal_strength(AntennaPosition2[0], AntennaPosition2[1]), signal_strength(AntennaPosition3[0], AntennaPosition3[1])])
+})
+
+
                      
 plt.scatter(receivers_df['position_x'], receivers_df['position_y'], c=receivers_df['signal_strength'],
-            cmap="gist_ncar", marker='o', s=20, alpha=0.5, label="antennas")
+            cmap="plasma", marker='o', edgecolors= "black", s=20, label="antennas")
  
 
 # Add colorbar
@@ -79,6 +74,8 @@ cid = fig.canvas.mpl_connect('button_press_event', on_click)
 
 plt.title('Radio Signal Strength (Click to Guess Transmitter Location)')
 plt.axis('equal')
+plt.xlim(0,37)
+plt.ylim(0,37)
 plt.xlabel('Position X') 
 plt.ylabel('Position Y')
 plt.legend()
