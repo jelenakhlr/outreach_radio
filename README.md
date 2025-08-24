523f453db4d3d6ba35539381716c791a6f854eaa
Jelena Petereit <108803123+jelenakhlr@users.noreply.github.com>
2025-08-25 00:23:50 +0200

    add gp300 sims

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GRAND Radio Signal Detection - Masterclass Code
 
-This repository contains Python code for a two-part masterclass on radio signal detection inspired by the GRAND experiment.
+This repository contains Python code for a masterclass on radio signal detection inspired by the GRAND experiment.
 
 authors:\
 Laetitia Megel, @Laetitia227\
@@ -8,13 +8,15 @@ Jelena Köhler, @jelenakhlr
 
 ## Learning Objectives:
 
-Part 1: Understand how an array of antennas can be used to locate a radio transmitter.\
-Part 2: Explore the concept of signal strength and how it varies with distance using few antennas.
+Part 0: Explore the concept of signal strength and how it varies with distance using few antennas.
+Part 1: Understand how the GP300 array of antennas can be used to locate a radio transmitter.\
+Part 2: Observe radio signals emitted from air showers on the GP300 array.
 
 ## Code Structure:
 
+```masterclass_part0.py```: Simulates radio signal detection using three individual antennas.
 ```masterclass_part1.py```: Simulates radio signal detection using a GP300 antenna layout.\
-```masterclass_part2.py```: Simulates radio signal detection using three individual antennas.
+```masterclass_part2.py''': Visualizes radio signals emitted from cosmic ray air showers for both time and signal strength distributions.
 
 ## Running the Code:
 
@@ -31,6 +33,8 @@ Navigate to the directory containing the code files.\
 Open a terminal window in the directory.\
 Run each part of the code using the following commands:
 
+    python masterclass_part0.py
+
     python masterclass_part1.py
     
     python masterclass_part2.py
@@ -40,13 +44,18 @@ Run each part of the code using the following commands:
 
 Each script provides a commented code example demonstrating the concepts mentioned in the learning objectives.
 
+### Part 0 - Three Individual Antennas:
+
+This part simulates the signal strength received by three individual antennas placed at different distances from the transmitter. The results are visualized to illustrate how signal strength weakens with increasing distance. Click on the position where you expect the transmitter to be located.
+
+
 ### Part 1 - GP300 Antenna Layout:
 
 This part simulates the detection of a radio transmitter using a grid of antennas similar to the GRAND experiment's GP300 layout. The code calculates the signal strength at each antenna based on its distance from the transmitter and visualizes the results. Click on the position where you expect the transmitter to be located.
 
-### Part 2 - Three Individual Antennas:
+### Part 2 - GP300 Air Showers:
 
-This part simulates the signal strength received by three individual antennas placed at different distances from the transmitter. The results are visualized to illustrate how signal strength weakens with increasing distance. Click on the position where you expect the transmitter to be located.
+This part visualizes a simulated cosmic ray event detected by the GP300 array. You can choose to display either the signal strength ("ADCfluence") distribution or the time distribution of the event.
 
 ## Further Exploration:
 
--- masterclass_part2.py
+++ masterclass_part2.py
@@ -1,117 +1,106 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from matplotlib import rc
+from matplotlib.gridspec import GridSpec
+from matplotlib.ticker import ScalarFormatter
+import matplotlib as mpl
+from matplotlib.widgets import RadioButtons
 
-# fancy plots
-rc("text", usetex=False)
-rc("font", size=11.0)
+# Set up Matplotlib styles
+rc("font", **{"family": "serif"})
+rc("font", size=12)
 
-fig, ax = plt.subplots()
-transmitter = np.array([np.random.randint(5, 35), np.random.randint(5, 35)])
+gp300 = np.genfromtxt("gp300.list", delimiter=" ")
+gp300_x = gp300[:, 2] * 10**-2  # convert from cm to m
+gp300_y = gp300[:, 3] * 10**-2  # convert from cm to m
 
-AntennaPosition1 = np.array([5, 15])
-AntennaPosition2 = np.array([15, 30])
-AntennaPosition3 = np.array([15, 5])
+def plot_event(event, event_number):
+    """
+    Plots the data for a single event and allows interactive clicks on dots.
 
-# Function to handle user clicks
-def on_click(event):
-    transmitter_x, transmitter_y = transmitter[0], transmitter[1]
-    clicked_x = event.xdata
-    clicked_y = event.ydata
-    distance_to_transmitter = np.sqrt(
-        ((clicked_x - transmitter_x) ** 2) + ((clicked_y - transmitter_y) ** 2)
-    )
-    threshold = (
-        5.0  # Adjust this value based on your plot scale and desired sensitivity
-    )
+    Args:
+        event (pd.DataFrame): The DataFrame containing event data.
+        event_number (int): The event ID to plot.
+    """
+    title = f"Event {event_number}"
+    
+    # Filter the data for the selected event
+    event_mask = (event["eventID"] == event_number) 
+    event_df = event[event_mask].copy()
+    energy = event_df["energy"].unique()[0]
+    theta = np.around(event_df["Zenith"].unique()[0],2)
+    phi = np.around(event_df["Azimuth"].unique()[0],2)
+    stations = event_df["stationID"].unique()
+    print(f"Event {event_number}, Energy: {energy / 1e9:.2e} GeV, {len(stations)} stations with signal")
 
-    if distance_to_transmitter <= threshold:
-        ax.scatter(
-            transmitter_x, transmitter_y, marker="x", color="r", label="transmitter"
-        )
-        text1 = "Correct! You found the transmitter location."
-        props = dict(boxstyle="round", facecolor="white")
-        ax.text(
-            0.0,
-            0.0,
-            text1,
-            transform=ax.transAxes,
-            verticalalignment="top",
-            bbox=props,
-        )
-        plt.legend()
+    # Add a variable to store the current type
+    type_options = ["ADCfluence", "time"]
+    type = [type_options[0]]  # Use a mutable object for closure
+    cmap = "RdPu" if type[0] == "ADCfluence" else "Blues"
+        
+    # Create the figure with a GridSpec layout
+    fig = plt.figure(figsize=(14, 14))
+    gs = GridSpec(5, 4, figure=fig)  # 4 columns: last for radio buttons
+    main_ax = fig.add_subplot(gs[:2, :3])  # Main plot spans top two rows, first 3 columns
 
-    else:
-        text2 = f"You clicked at: ({clicked_x:.2f}, {clicked_y:.2f})\n Try again! The transmitter might be closer to one of the stronger signal areas."
-        props = dict(boxstyle="round", facecolor="white")
-        ax.text(
-            0.0,
-            0.0,
-            text2,
-            transform=ax.transAxes,
-            verticalalignment="top",
-            bbox=props,
-        )
+    # Add radio buttons for type selection
+    radio_ax = fig.add_axes([0.82, 0.7, 0.12, 0.12])
+    radio = RadioButtons(radio_ax, type_options)
+    radio_ax.set_title("type", fontsize=12)
 
-    fig.canvas.draw_idle()  # Trigger redrawing
-    fig.canvas.flush_events()  # Process events for a smooth update
-    return True
-
-
-def signal_strength(antenna_position_x, antenna_position_y):
-    distance = np.sqrt(
-        (transmitter[0] - antenna_position_x) ** 2
-        + (transmitter[1] - antenna_position_y) ** 2
-    )
-    signal_strength = 1 / distance
-    return signal_strength
+    if type[0] == "ADCfluence":
+        norm = mpl.colors.LogNorm(vmin=max(event_df["ADCfluence"].min(), 1e-2), vmax=event_df["ADCfluence"].max())
+        cmap = "RdPu"
+        cbar_label = "signal strength"
+    elif type[0] == "time":
+        norm = plt.Normalize(vmin=event_df["time"].min(), vmax=event_df["time"].max())
+        cmap = "Blues"
+        cbar_label = "time in ns"
 
 
-receivers_df = pd.DataFrame(
-    {
-        "position_x": np.array(
-            [AntennaPosition1[0], AntennaPosition2[0], AntennaPosition3[0]]
-        ),
-        "position_y": np.array(
-            [AntennaPosition1[1], AntennaPosition2[1], AntennaPosition3[1]]
-        ),
-        "signal_strength": np.array(
-            [
-                signal_strength(AntennaPosition1[0], AntennaPosition1[1]),
-                signal_strength(AntennaPosition2[0], AntennaPosition2[1]),
-                signal_strength(AntennaPosition3[0], AntennaPosition3[1]),
-            ]
-        ),
-    }
-)
+    main_ax.scatter(gp300_x, gp300_y, color="grey", s=20, alpha=0.5)
+    sc = main_ax.scatter(event_df['positionX'], event_df['positionY'], c=event_df[f"{type[0]}"],
+                         marker='o', s=50, norm=norm, cmap=cmap)
+    main_ax.set_xlabel('x position in m')
+    main_ax.set_ylabel('y position in m')
+    main_ax.set_title(f'{title} - Event {event_number}\nE={energy:.2e} eV, ' + rf'$\theta$={theta}, $\varphi$={phi}')
+
+    # Add a colorbar
+    cbar = fig.colorbar(sc, ax=main_ax, orientation='vertical')
+    cbar.set_label(cbar_label)
+
+    def update_type(label):
+        type[0] = label
+        if type[0] == "ADCfluence":
+            current_cmap = "RdPu"
+            # Avoid log(0) by setting a small positive vmin
+            vmin = max(event_df["ADCfluence"].min(), 1e-2)
+            norm = mpl.colors.LogNorm(vmin=vmin, vmax=event_df["ADCfluence"].max())
+            cbar_label = "signal strength"
+        elif type[0] == "time":
+            current_cmap = "Blues"
+            norm = plt.Normalize(vmin=event_df["time"].min(), vmax=event_df["time"].max())
+            cbar_label = "time in ns"
+
+        sc.set_array(event_df[f"{type[0]}"])
+        sc.set_norm(norm)
+        sc.set_cmap(current_cmap)
+        cbar.set_label(cbar_label)
+        cbar.update_normal(sc)
+        fig.canvas.draw_idle()
+
+    radio.on_clicked(update_type)
+    plt.show()
 
 
-plt.scatter(
-    receivers_df["position_x"],
-    receivers_df["position_y"],
-    c=receivers_df["signal_strength"],
-    cmap="plasma",
-    marker="o",
-    edgecolors="black",
-    s=40,
-    label="antennas",
-)
-
-
-# Add colorbar
-cbar = plt.colorbar()
-cbar.set_label("signal strength")
-
-
-# Call `on_click` when user clicks anywhere on the plot
-cid = fig.canvas.mpl_connect("button_press_event", on_click)
-
-plt.title("Radio Signal Strength (Click to Guess Transmitter Location)")
-plt.axis("equal")
-plt.xlim(0, 37)
-plt.ylim(0, 37)
-plt.xlabel("Position X")
-plt.ylabel("Position Y")
-plt.legend()
-plt.show()
+if __name__ == "__main__":
+    # Load the data
+    file = pd.read_csv("/Users/jelenapetereit/work/GRAND_DB/database.csv")
+    events = sorted(file["eventID"].unique())
+    # Select an event to display
+    print("Available events:", events)
+    event_number = int(input("Enter the event ID to display: "))
+    
+    # Plot the selected event
+    plot_event(file, event_number)
